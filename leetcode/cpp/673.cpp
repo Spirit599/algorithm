b1d42d8782b0a408ec5ab4f4744f37f539fbcd9f
jiangxy <jiangxy@demo.com>
2022-11-14 16:02:46 +0800

    2022-11-14 16:02:46

--- leetcode/cpp/673.cpp
+++ leetcode/cpp/673.cpp
@@ -4,8 +4,26 @@ public:
 
         int ans = 0;
         map<int, int, greater<int>> memo;
-
         int n = nums.size();
-        
+        pair<int, int> dp[n];
+        for(int i = 0; i < n; ++i)
+        {
+            map<int, int, greater<int>> cur;
+            cur[0] = 1;
+            for(int j = 0; j < i; ++j)
+            {
+                if(nums[j] < nums[i])
+                    cur[dp[j].first] += dp[j].second;
+            }
+            auto it = cur.begin();
+            // for(auto& kv : cur)
+            //     printf("%d %d | ", kv.first,kv.second);
+            // printf("\n");
+            memo[it->first + 1] += it->second;
+            dp[i] = make_pair(it->first + 1, it->second);
+            // printf("dp %d %d\n", it->first + 1, it->second);
+        }
+
+        return memo.begin()->second;
     }
 };
