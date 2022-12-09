c8d23ba92d6d8e92fdf8a8dcb56c4ea7367ce03f
jiangxy <jiangxy@demo.com>
2022-12-09 14:47:13 +0800

    2022-12-09 14:47:13

---
+++ leetcode/cpp/1780_1.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    bool checkPowersOfThree(int n) {
+
+        while(n)
+        {
+            if(n % 3 == 2)
+                return false;
+            n = n / 3;
+        }
+        return true;
+    }
+};
